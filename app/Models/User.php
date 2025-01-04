01455c61dc4775adbe38194293d84176527b7740
Lee <elacamel321@gmail.com>
2025-01-04 08:19:30 +0800

    added user table migrations, modified columns

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,12 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'contact',
+        'dob',
+        'company',
+        'gender',
+        'status',
+        'role',
     ];
 
     /**
